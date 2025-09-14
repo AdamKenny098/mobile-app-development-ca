364de46e0e1bf31b30258b9b55bb568408e7daaa
AdamKenny <122899668+AdamKenny098@users.noreply.github.com>
2025-09-14 02:17:40 +0100

    Lab K02 Created the Game Model Added Game Creation Added Game Reading Added Game Updatability Added Game Searching by id

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,26 @@
-package org.setu.placemark
+package org.setu.placemark.console.main
 
 import mu.KotlinLogging
+import org.setu.placemark.console.main.models.GameModel
 
 private val logger = KotlinLogging.logger {}
 
+val games = ArrayList<GameModel>()
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    logger.info { "Launching Gam Console App" }
-    println("Game Kotlin App Version 1.0")
+    logger.info { "Launching Game Console App" }
+    println("Game Kotlin App Version 2.0")
 
     var input: Int
 
     do {
         input = menu()
         when(input) {
-            1 -> println("You Chose Add Game")
+            1 -> addGame()
+            2 -> updateGame()
+            3 -> listGames()
+            4 -> searchGame()
+            -99 -> dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -25,23 +29,106 @@ fun main() {
     logger.info { "Shutting Down Game Console App" }
 }
 
-
-fun menu():Int
-{
+fun menu() : Int {
     var option : Int
-    var input: String? = null
+    var input: String?
 
-    println("Main Menu")
+    println("MAIN MENU")
     println(" 1. Add Game")
     println(" 2. Update Game")
     println(" 3. List All Games")
+    println(" 4. Search Games")
     println("-1. Exit")
     println()
-    print("Enter an integer : ")
+    print("Enter Option : ")
     input = readLine()!!
-    option = if (input.toIntOrNull() != null && !input.isEmpty())
+    option = if (input.toIntOrNull() != null && input.isNotEmpty())
         input.toInt()
     else
         -9
     return option
+}
+
+fun addGame(){
+    var aGame = GameModel()
+    println("Add Game")
+    println()
+    print("Enter a Title : ")
+    aGame.title = readLine()!!
+    print("Enter a Description : ")
+    aGame.description = readLine()!!
+    print("Enter an Age Rating (number) : ")
+    aGame.ageRating = readLine()!!.toIntOrNull() ?: 0
+
+    if (aGame.title.isNotEmpty() && aGame.description.isNotEmpty()) {
+        aGame.id = games.size.toLong()
+        games.add(aGame.copy())
+        logger.info("Game Added : [ $aGame ]")
+    }
+    else
+        logger.info("Game Not Added")
+}
+
+fun updateGame() {
+    println("Update Game")
+    println()
+    listGames()
+    var searchId = getId()
+    val aGame = search(searchId)
+
+    if(aGame != null) {
+        print("Enter a new Title for [ " + aGame.title + " ] : ")
+        aGame.title = readLine()!!
+        print("Enter a new Description for [ " + aGame.description + " ] : ")
+        aGame.description = readLine()!!
+        print("Enter a new Age Rating for [ " + aGame.ageRating + " ] : ")
+        aGame.ageRating = readLine()!!.toIntOrNull() ?: aGame.ageRating
+
+        println(
+            "You updated [ " + aGame.title + " ] for title, " +
+                    "[ " + aGame.description + " ] for description, " +
+                    "and [ " + aGame.ageRating + " ] for age rating"
+        )
+    }
+    else
+        println("Game Not Updated...")
+}
+
+fun listGames() {
+    println("List All Games")
+    println()
+    games.forEach { println("ID: ${it.id}, Title: ${it.title}, Desc: ${it.description}, Age Rating: ${it.ageRating}") }
+}
+
+fun searchGame() {
+    var searchId = getId()
+    val aGame = search(searchId)
+
+    if(aGame != null)
+        println("Game Details [ ID: ${aGame.id}, Title: ${aGame.title}, Desc: ${aGame.description}, Age Rating: ${aGame.ageRating} ]")
+    else
+        println("Game Not Found...")
+}
+
+fun getId() : Long {
+    var strId : String? // String to hold user input
+    var searchId : Long // Long to hold converted id
+    print("Enter id to Search/Update : ")
+    strId = readLine()!!
+    searchId = if (strId.toLongOrNull() != null && strId.isNotEmpty())
+        strId.toLong()
+    else
+        -9
+    return searchId
+}
+
+fun search(id: Long) : GameModel? {
+    var foundGame: GameModel? = games.find { g -> g.id == id }
+    return foundGame
+}
+
+fun dummyData() {
+    games.add(GameModel(1, "Overwatch", "A first person team based hero shooter", 12))
+    games.add(GameModel(2, "Fallout 4", "A post nuclear apocalypse survival RPG", 18))
+    games.add(GameModel(3, "Snake", "A classic", 3))
 }
---
+++ src/main/kotlin/models/GameModel.kt
@@ -0,0 +1,7 @@
+package org.setu.placemark.console.main.models
+
+data class GameModel(
+                     var id: Long = 0,
+                     var title: String = "",
+                     var description: String = "",
+                     var ageRating: Int = 0)
